ab6eca135dd6bf50da7f5d29d1b0755c5e727252
BuddingIntents <79275023+buddingintents@users.noreply.github.com>
2025-08-24 14:11:42 +0530

    Create init.py

---
+++ pages/init.py
@@ -0,0 +1,5 @@
+# Import all page modules
+from .prompt_page import render as prompt_page
+from .custom_page import render as custom_page  
+from .statistics_page import render as statistics_page
+from .responses_page import render as responses_page
